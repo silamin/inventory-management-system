567f5a579abecf7a6418bb5f246dfc3608d827f8
Amin Aouina <91031103+silamin@users.noreply.github.com>
2024-12-22 00:51:17 +0100

    pick up warehouse worker

---
+++ BlazorServerApp/BlazorServerApp/Managers/PickupManager.cs
@@ -0,0 +1,36 @@
+﻿using BlazorServerApp.Application.UseCases;
+using Orders;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerApp.Managers
+{
+    public class PickupManager
+    {
+        private readonly OrderUseCases _orderUseCases;
+
+        public PickupManager(OrderUseCases orderUseCases)
+        {
+            _orderUseCases = orderUseCases;
+        }
+
+        public List<Order> Orders { get; private set; } = new();
+        public bool IsLoading { get; private set; } = true;
+
+        public async Task LoadOrdersAsync()
+        {
+            IsLoading = true;
+            try
+            {
+                Orders = (await _orderUseCases.GetOrdersByStatusAsync(OrderStatus.InProgress)).ToList();
+            }
+            finally
+            {
+                IsLoading = false;
+            }
+        }
+
+
+    }
+}
